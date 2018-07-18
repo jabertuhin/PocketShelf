36644423e18659efd83106e492eae67b243d0ef5
Jaber <jabertuhin@gmail.com>
2018-07-18 17:19:29 +0600

    Just creating second intent

--- app/src/main/java/com/example/tuhin/pocketshelf/my_book_shelf.java
+++ app/src/main/java/com/example/tuhin/pocketshelf/my_book_shelf.java
@@ -13,7 +13,7 @@ public class my_book_shelf extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.my_book_shelf);
 
     }
 }
