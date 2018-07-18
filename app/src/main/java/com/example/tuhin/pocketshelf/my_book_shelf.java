f1fc476e6f0c2df3ebd9c13281907d7ab723f9d1
Jaber <jabertuhin@gmail.com>
2018-07-18 16:13:29 +0600

    Just creating second intent

--- app/src/main/java/com/example/tuhin/pocketshelf/my_book_shelf.java
+++ app/src/main/java/com/example/tuhin/pocketshelf/my_book_shelf.java
@@ -3,18 +3,17 @@ package com.example.tuhin.pocketshelf;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class my_book_shelf extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void toMyShelf(View view) {
-        Intent goThere = new Intent(this,my_book_shelf.class);
-        startActivity(goThere);
     }
 }
