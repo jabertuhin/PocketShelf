368d5d7e1987549ca4cd899e5aa9bc1600698028
Jaber <jabertuhin@gmail.com>
2018-07-19 10:10:43 +0600

    Just creating Want To Buy Books Intent.

---
+++ app/src/main/java/com/example/tuhin/pocketshelf/want_to_buy.java
@@ -0,0 +1,12 @@
+package com.example.tuhin.pocketshelf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class want_to_buy extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.want_to_buy);
+    }
+}
